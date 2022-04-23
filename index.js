ee999f33367b08af9a1f655d23026823ef52da35
sourav1087c <40231873+sourav1087c@users.noreply.github.com>
2022-04-23 14:02:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+// requiring library
+require('dotenv').config();
+const express = require('express');
+const db = require('./config/mongoose');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const User = require('./model/user');
+const Movie = require('./model/movie');
+
+const app = express();
+
+//////////////////////////// Middleware /////////////////////////
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+async function APIDRAMA() {
+    // const dummy = await User.find({});
+    const movieCheck = await Movie.find({title: "Ice Age: Continental Drift"});
+    if(!movieCheck[0]){
+        //////////////////// Make a request for a user with a given ID///////////
+        const api = [
+            `https://api.themoviedb.org/4/list/10?page=1&api_key=${process.env.API_KEY}`,
+            `https://api.themoviedb.org/4/list/10?page=2&api_key=${process.env.API_KEY}`,
+            `https://api.themoviedb.org/4/list/10?page=3&api_key=${process.env.API_KEY}`
+        ]
+        api.forEach((link)=>{
+        axios.get(link)
+            .then( function (response) {
+             response.data.results.forEach(async (element, index)=>{
+                try{
+                await Movie.create(element);
+                // console.log("Data Fetched Successfully ", index);
+                }catch(err){
+                console.log('Error ', err);
+                }
+            });
+            })
+            .catch(function (error) {
+            // handle error
+            console.log(error);
+            });
+        });
+        
+    }
+}
+APIDRAMA();
+/////////////////////// Route Middleware ///////////////////////
+app.use('/', require('./routes'));
+
+
+
+app.listen(3000, (err)=>{
+    if(err){console.log("Error",err);}
+    console.log("Server is up and running in PORT 3000");
+})
